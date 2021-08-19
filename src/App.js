b8b4f447871091341c829f8aa8569caefeff7de9
vardhan <harshwardhankalse03@gmail.com>
2021-08-19 16:50:25 +0530

    App.js and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import React, { useState } from "react";
+import ToolBar from "./components/ToolBar";
+import Title from "./components/Title";
+// comps conditional
+import Paragrph from "./components/Paragraph";
+import SubTitle from "./components/SubTitle";
+import AddImage from "./components/AddImage";
+import AddCode from "./components/AddCode";
 
 function App() {
-  return <div className="App"></div>;
+  const [paragrph, setParagrph] = useState(false);
+  const [subtitle, setSubtitle] = useState(false);
+  const [addImage, setAddImage] = useState(false);
+  const [addCode, setAddCode] = useState(false);
+  const [isClick, setIsClick] = useState(false);
+
+  return (
+    <div className="App">
+      <Title />
+      <div className="blog-content">
+        {paragrph && <Paragrph setParagrph={setParagrph} />}
+        {subtitle && <SubTitle setSubtitle={setSubtitle} />}
+        {addImage && <AddImage isClick={isClick} />}
+        {addCode && <AddCode setAddCode={setAddCode} />}
+      </div>
+      <ToolBar
+        isClick={isClick}
+        setIsClick={setIsClick}
+        setParagrph={setParagrph}
+        setAddCode={setAddCode}
+        setSubtitle={setSubtitle}
+        setAddImage={setAddImage}
+      />
+    </div>
+  );
 }
 
 export default App;
