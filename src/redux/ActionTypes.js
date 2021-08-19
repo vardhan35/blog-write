7a17002d7f03f07f3f87a8e91a389e5ed7f7d467
vardhan <harshwardhankalse03@gmail.com>
2021-08-19 16:54:34 +0530

    Redux action Types

---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_TITLE = "ADD_TITLE";
+export const ADD_PARAGRAPH = "ADD_PARAGRAPH";
+export const ADD_SUBTITLE = "ADD_SUBTITLE";
+export const ADD_IMAGE = "ADD_IMAGE";
+export const ADD_CODE = "ADD_CODE";
